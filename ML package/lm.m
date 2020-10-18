637d5646584d22e20d8dc934b855fe53ef9e6e9b
Nathan Krumholz <krumh010@umn.edu>
2020-10-17 19:25:05 -0500

    linear model now returns pvalue of each fitted term instead of just t score

--- ML package/lm.m
+++ ML package/lm.m
@@ -1,21 +1,33 @@
-function [theta, history, t] = lm(X, y, alpha, num_iters)
+function [theta, err, t] = lm(X, y, alpha, num_iters)
 % Multivariate linear Regression
 %
-% Syntax: [theta, history] = lm(X, y, theta, alpha, num_iters)
+% Syntax: [theta, err, fit] = lm(X, y, theta, zero)
 %
-% Description
+% Description:
+% Linear Regression 
+%Outputs: 
+% theta = Fit Coeficients 
+% 
+
 
     m = length(y); % number of training examples 
-    X = [ones(m,1), X]; % Add a column of ones to X
+    if ~exist('zero','var') 
+        zero = false;
+    end
+    
+    if zero == false
+        X = [ones(m,1), X]; % Add a column of ones to X too add bias
+    end
+
     [a,b] = size(X); 
     theta = zeros(b, 1); % Initialize fitting parameters
+
     if m == a
-        history = zeros(num_iters, 1);
-        for iter = 1:num_iters
-            hx = X*theta-y;
-            theta = theta - X'*hx*(alpha/m);
-            history(iter) = (hx'*hx)/m/2;
-        end
+        % Regression through Normal Equation
+        hx = X*theta-y;
+        theta = pinv(X' * X) * X' * y;
+        history = (hx'*hx)/m/2;
+
         %compute t statistic for regression line: 
         sigma = sum(y.^2);
         for i = 1:b
@@ -24,8 +36,15 @@ function [theta, history, t] = lm(X, y, alpha, num_iters)
         end
         sigma = sqrt(sigma/(a-b));
         SXX = sum(X.^2) - (sum(X).^2./m);
-        t = sigma./sqrt(SXX);
+        s_ehat = sigma./sqrt(SXX);
+        t = theta' ./ s_ehat;
+        t = t(1) = NaN;
         
+        %compute p value from t statistic 
+        nd = a-b;
+        p = 2 * (1-student_cdf(abs(t),repelem(a-b,length(t))));
+        vars = {'coefs', 't value', 'Pr(>|t|)'};
+        fit = table(theta,t',p','VariableNames',vars);
     else
         disp("Need equal number of observations (X) and labels (y)")
     end
---
+++ Stats package/student_cdf.m
@@ -0,0 +1,26 @@
+function p = student_cdf(t,nd)
+% Syntax: p = student_cdf(t,nd)
+%
+%Ouput: 
+% P = One-tailed probability (left tail) 
+%
+%Inputs: 
+% t = Student t-test score
+% nd = degrees of freedom
+    
+if nd <=0
+    error('Degrees of freedom must be positive.');
+end
+
+%check for Nan's 
+if any( not(isfinite(t(:))) | not(isfinite(nd(:))) )
+    sel = find(isfinite(t) & isfinite(nd));
+    x = nan(size(t));
+    p = nan(size(t));
+    x(sel) = t(sel).^2 ./ (nd(sel) + t(sel).^2);
+    p(sel) = 0.5 .* ( 1 + sign(t(sel)) .* betainc( x(sel), 0.5, 0.5*nd(sel)) );
+else
+    x = t.^2 ./ (nd + t.^2) ;
+    p = 0.5 .* ( 1 + sign(t) .* betainc( x, 0.5, 0.5*nd ) );
+end 
+end
