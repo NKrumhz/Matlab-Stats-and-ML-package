ad5ccc3b0a7493b6f684c71897e355de2167ed98
Nathan Krumholz <krumh010@umn.edu>
2021-01-01 00:32:43 -0600

    auto part of auto_trig added

--- DSP package/auto_trig.m
+++ DSP package/auto_trig.m
@@ -1,20 +1,33 @@
-function trig = auto_trig(sig, alpha, method)
+function trig = auto_trig(sig, method, alpha)
 %auto_trig - Description
 %%
 %inputs: 
-% Data 
-% alpha 
-% method 
+% sig = input Data
+% method = returns signals greater than alpha when positive, 
+%less than alpha when negative and equal to alpha when 0. 
+%alpha is also scaled by the value of method. 
+% alpha = the numeric threshold to restart the signal.
+%if not input, the min value will be used if method is 
+%positive, the max value will be used if method is negative. 
+
 if(istable(sig))
     sig = table2array(sig).';
 end
 
-if(method >= 1)
-    trig = sig > alpha;
+if(method > 0)
+    % use in value if alpha is not supplied
+    if ~exist('alpha','var') 
+        alpha = min(sig);
+    end
+    trig = sig > abs(method) * alpha;
 elseif(method == 0)
     trig = sig == alpha;
-elseif(method <= -1)
-    trig = sig < alpha;
-
+elseif(method < 0)
+    % use max value if alpha is not supplied
+    if ~exist('alpha','var') 
+        alpha = max(sig);
+    end
+end
+    trig = sig < abs(method) * alpha;
 trig = 1 + find(diff(trig)>0);
 end
